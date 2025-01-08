4a761ffaba90bc1fe9bb64150fd897587cf3754e
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-08 16:45:52 +0300

    Add project files.

---
+++ Practice_Create_Class/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Create_Class
+{
+    internal class Person
+    {
+        public bool Student { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string BirthDate { get; set; }
+
+        public void Introduce()
+        {
+            if (Student == true)
+            {
+                Console.WriteLine("\nStudent Information:");
+            }
+            else
+            {
+                Console.WriteLine("\nTeacher Information:");
+            }
+
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Surname: " + Surname);
+            Console.WriteLine("Date of Birth: " +  BirthDate);
+
+        }
+
+    }
+
+}
+
---
+++ Practice_Create_Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using Practice_Create_Class;
+
+Person teacher1 = new Person()
+{
+    Student = false,
+    Name = "Till",
+    Surname = "Lindemann",
+    BirthDate = "06.01.1963"
+
+};
+
+Person teacher2 = new Person()
+{
+    Student= false,
+    Name = "Can",
+    Surname = "Temiz",
+    BirthDate = "13.09.1987"
+};
+
+Person student1 = new Person()
+{
+    Student = true,
+    Name = "Abraham",
+    Surname = "Joseph",
+    BirthDate = "22.10.1992"
+};
+
+Person student2 = new Person()
+{
+    Student = true,
+    Name = "Austin",
+    Surname = "Post",
+    BirthDate = "04.07.1995"
+};
+
+teacher1.Introduce();
+teacher2.Introduce();
+student1.Introduce();
+student2.Introduce();
